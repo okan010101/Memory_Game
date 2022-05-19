9109bacfdbe11dcff806ffeb9d9808c13eada6ab
Okan Aşcıoğlu <80708651+okan010101@users.noreply.github.com>
2022-05-19 11:22:10 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1> Memory Game</h1>
+      <Cards></Cards>
+      {/* <div className="container"></div> */}
+      
+    </div>
+  );
+}
+
+export default App;
